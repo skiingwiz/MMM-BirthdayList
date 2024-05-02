2be3c27e6ecaeddb36ba2d503c5436ec38fa67c0
skiingwiz <skiingwiz@gmail.com>
2024-05-01 22:33:53 -0400

    spaces not tabs

--- MMM-BirthdayList.js
+++ MMM-BirthdayList.js
@@ -1,38 +1,38 @@
 Module.register("MMM-BirthdayList", {
 
-	// Default module config.
-	defaults: {
-		tableClass: "small",
+    // Default module config.
+    defaults: {
+        tableClass: "small",
         birthdays: []
-	},
+    },
 
-	// Define required scripts.
-	getScripts: function () {
-		return [];
-	},
+    // Define required scripts.
+    getScripts: function () {
+        return [];
+    },
 
-	// Define required stylescripts.
-	getStyles: function () {
-		return ["MMM-BirthdayList.css"];
-	},
+    // Define required stylescripts.
+    getStyles: function () {
+        return ["MMM-BirthdayList.css"];
+    },
 
-	// Define start sequence.
-	start: function () {
-		Log.info("Starting module: " + this.name);
-		this.label = "";
+    // Define start sequence.
+    start: function () {
+        Log.info("Starting module: " + this.name);
+        this.label = "";
         this.setRefreshTimer()
-	},
+    },
 
-	// Override dom generator.
-	getDom: function () {
-		var today = new Date();
+    // Override dom generator.
+    getDom: function () {
+        var today = new Date();
         var year = today.getYear() + 1900;
         var month = (today.getMonth() + 1)
         var day = today.getDate()
-		var wrapper = document.createElement("div");
+        var wrapper = document.createElement("div");
 
-		var table = document.createElement("table");
-		table.className = this.config.tableClass;
+        var table = document.createElement("table");
+        table.className = this.config.tableClass;
 
         for(item in this.config.birthdays) {
           var bday = this.config.birthdays[item]
@@ -51,26 +51,26 @@ Module.register("MMM-BirthdayList", {
               ext = 'th';
             }
 
-	        var labelRow = document.createElement("tr");
-	        table.appendChild(labelRow);
+            var labelRow = document.createElement("tr");
+            table.appendChild(labelRow);
 
-		    var labelItem = document.createElement("td");
+            var labelItem = document.createElement("td");
             var balloons = '<img src="modules/MMM-BirthdayList/balloons.png" class="bday-image" />'
-		    labelItem.innerHTML = `${balloons}Happy ${age}${ext} Birthday ${bday.name}!!${balloons}`
-		    labelRow.appendChild(labelItem);
+            labelItem.innerHTML = `${balloons}Happy ${age}${ext} Birthday ${bday.name}!!${balloons}`
+            labelRow.appendChild(labelItem);
           }
         }
 
-		return table;
-	},
+        return table;
+    },
 
     setRefreshTimer: function () {
         var now = new Date();
         var time = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
         var ms = time - now;
-		setInterval(function () {
-			this.updateDom();
+        setInterval(function () {
+            this.updateDom();
             this.setRefreshTimer();
-		}, ms);
+        }, ms);
     }
 });
